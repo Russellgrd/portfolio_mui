f60cd19a2e5c3b1944b7ac34a0878b82a5e230d4
unknown <russell_driver@ymail.com>
2023-03-15 07:03:21 +0000

    final commit before deploy

--- src/App.js
+++ src/App.js
@@ -28,8 +28,8 @@ const App = () => {
                 <Route path="/" element={<Home />} />
                 <Route path="/about" element={<About />} />
                 <Route path="/projects" element={<Projects />} />
-                <Route path="/contact" element={<Contact />} />
                 <Route path="/resume" element={<Resume />} />
+                <Route path="/contact" element={<Contact />} />
             </Routes>
             <Footer />
         </Box>
